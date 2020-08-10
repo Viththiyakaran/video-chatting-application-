e125bbf3e366d562ffcbd3f4352784c728af0dca
Viththiyakaran <battikaran6@gmail.com>
2020-08-10 09:31:34 +0530

    Setup ejs

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 const server = require('http').Server(app);
 
 
+
+app.set('view engine','ejs');
+
+
 app.get('/', (req,res )=>{
-    res.status(200).send("Hello world");
+    res.render("room");
 })
 
 
